357dea5a14b51e24ba761e54ba27559b82ef4bef
matvei verkhovtsev <m.mavhhh@gmail.com>
2024-08-26 19:57:13 +0300

    feat: add request tot the server

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import "./App.css";
 import "./scss/app.scss";
 
+import React from "react";
 import { Header } from "./components/Header";
 import { Categories } from "./components/Categories";
 import { PizzaBlock } from "./components/PizzaBlock";
 import { Sort } from "./components/Sort";
 
-import pizzas from "../src/assets/pizzas.json";
-
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://66ccaf20a4dd3c8a71b87c41.mockapi.io/Items")
+      .then((res) => res.json())
+      .then((json) => setItems(json));
+  }, []);
+
   return (
     <div className="App">
       <div className="wrapper">
@@ -22,7 +29,7 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {pizzas.map((obj, index) => (
+              {items.map((obj, index) => (
                 <PizzaBlock {...obj} />
               ))}
             </div>
